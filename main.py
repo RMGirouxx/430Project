9fa8f3c04956e6ac1b8449e6d22eaee7db2677ac
RMGirouxx <99826238+RMGirouxx@users.noreply.github.com>
2022-12-11 22:09:42 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-import csv
-import sys
 import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
 
-original_df = pd.read_csv('languages.csv')
-split_df = pd.DataFrame()
+# Import .csv into python
+df = pd.read_csv("languagesSplit.csv", header=None)
 
-with open(original_df, 'r') as csv_in_file:
-    with open(split_df, 'w') as csv_out_file:
-        reader = csv.reader(csv_in_file, delimiter=';')
-        writer = csv.writer(csv_out_file, delimiter=',')
-        for row in reader:
-            print(row)
-            writer.writerow(row)
+# Fill 'nan' values with '0's for later evaluation
+df = df.fillna('0')
 
+# make a list of each unique item in the dataset
+uniqueItems = pd.unique(df[0])
+
+# convert dataset to string list
+languages = df.astype(str).values.tolist()
+
+# convert list into boolean array and back into dataset for apriori
+transactions = TransactionEncoder()
+tranArr = transactions.fit(languages).transform(languages)
+df = pd.DataFrame(tranArr, columns=transactions.columns_)
+
+# initiate apriori function
+langPairs = apriori(df, min_support=0.3, use_colnames=True, max_len=2)
+
+# create row displaying length of pairs
+langPairs['length'] = langPairs['itemsets'].apply(lambda x: len(x))
+
+# set max support and length for evaluation
+langPairs = langPairs[(langPairs['length'] == 2) &
+                      (langPairs['support'] <= .6)]
+
+# remove any frequent itemsets that contain a '0' as one value in the tuplet
+for i in range(len(df.columns)):
+    if i != '0':
+        langPairs = langPairs[langPairs['itemsets'] != {'0', uniqueItems[i]}]
+        langPairs = langPairs[langPairs['itemsets'] != {uniqueItems[i], '0'}]
+
+# print apriori results
+print(langPairs)
